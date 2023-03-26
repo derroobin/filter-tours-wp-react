5bbfee28e611b1d10a364dd6f21fc589cb9c20f5
DerRoobin <906113+derroobin@users.noreply.github.com>
2023-03-26 11:36:53 +0200

    rename root

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(
+  document.getElementById('touren-react') as HTMLElement
+).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
